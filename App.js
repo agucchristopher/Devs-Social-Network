979cdb72c06849a0fa8c1d6590c90895df7af7fc
Agu C Christopher <aguchris740@gmail.com>
2023-01-13 21:34:36 +0100

    added packages

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+// import { } from 'stream-chat-expo'
+import { StreamChat } from "stream-chat";
 
 export default function App() {
+  const client = StreamChat.getInstance("a9n9yt3mjp5w");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
